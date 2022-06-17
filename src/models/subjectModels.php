a74efe9cc1fc26810813ed31a28f54a5e5e6f80d
Nguyễn Văn Huy <nvhuy0405@gmail.com>
2022-06-17 20:41:47 +0700

    view_class

--- src/models/subjectModels.php
+++
@@ -1,9 +0,0 @@
-<?php 
-class subjectModels extends ConnectDB{
-    // Lấy tất cả dữ liệu từ bảng subject
-    function getAllValues(){
-        $sql = "SELECT * FROM `subject`";
-        $subJect = mysqli_query($this->connection,$sql);
-        return $subJect;
-    }
-}
