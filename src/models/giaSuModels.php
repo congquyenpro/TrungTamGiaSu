8c77ad07ee1dcb6f9b2b039b2289ee655d5cc227
ND-Tuan <93861138+ND-Tuan@users.noreply.github.com>
2022-06-18 22:40:41 +0700

    update giaSuModels.php

--- src/models/giaSuModels.php
+++ src/models/giaSuModels.php
@@ -40,3 +40,61 @@ class giaSuModels extends ConnectDB{
             return false;
         }
     }
+    // Đổi mật khẩu
+    function ChangePass($password,$secure_pass){
+        $id = $_SESSION['id'];
+        // tìm kiếm dữ liệu qua id
+        $user = $this->selectOne('id', $id);
+        // Kiểm tra xem dữ liệu có tồn tại hay không
+        if (isset($user['password'])){
+            // Kiểm tra mật khẩu
+            $verify = password_verify($password, $user['password']);
+            if ($verify){
+                // Truy vấm cập nhập
+                $sql = "UPDATE
+                        `instructor`
+                    SET
+                        `password` = '$secure_pass'
+                    WHERE
+                        `id` = '$id'";
+                mysqli_query($this->connection,$sql);
+                return true;
+            }  
+        }else{
+            return false;
+        }
+    }
+    // Hàm chọn 1 hàng dữ liệu
+    function selectOne($key,$value){
+        $sql = "SELECT * FROM `instructor` WHERE `$key` = '$value'";
+        $user = mysqli_query($this->connection,$sql);
+        // Ép dữ liệu từ mảng về biên
+        $user = mysqli_fetch_array($user);
+        return $user;
+    }
+    // Hàm update
+    function updateOne($id, $name, $email, $phone_number, $gender, $avatar, $school_level, $subject, $address, $description){
+        // Câu truy vấn
+        $sql = "UPDATE
+                    `instructor`
+                SET
+                    `name` = '$name',
+                    `email` = '$email',
+                    `phone_number` = '$phone_number',
+                    `gender` = '$gender',
+                    `avatar` = '$avatar',
+                    `school_level`  = '$school_level',
+                    `subject`   = '$subject',
+                    `address` = '$address',
+                    `description` = '$description'
+                WHERE
+                    `id` = '$id'";
+        // Thực hiện truy vấn và kiểm tra
+        mysqli_query($this->connection,$sql);
+        if (mysqli_error($this->connection) == ""){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
