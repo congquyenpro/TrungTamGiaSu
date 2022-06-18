29ecc2e6cb00076267e53478ced8bf6f7b373e72
DuanLee7662 <93870205+DuanLee7662@users.noreply.github.com>
2022-06-18 21:34:51 +0700

    update Account information interface

--- src/views/content/user/myAccount.php
+++ src/views/content/user/myAccount.php
@@ -25,5 +25,74 @@
                         </a>
                 </div>
             </div>
+            <div class="box-info">
+                <div id="box-view" class="box-info-view">
+                    <div class="name-header">
+                        Hồ sơ thông tin
+                    </div>
+                    <?php
+                        if (isset($_SESSION['done'])){
+                            echo "<span style='color: blue;'>Cập nhập tài khoản thành công</span>";
+                            unset($_SESSION['done']);
+                        }else if (isset($_SESSION['error'])){
+                            echo "<span style='color: blue;'>Lỗi trùng Email hoặc lỗi file</span>";
+                            unset($_SESSION['done']);
+                        } 
+                    ?>
+                    <div id="my-infor">
+                        <div class="avatar-preview">
+                            <img src="<?php echo $actual_link ?>/public/images/avatar/<?php echo $_SESSION['avatar']?>" alt="<?php echo $_SESSION['name']?>">
+                        </div>
+                        <ul>
+                            <li>
+                                <span style="color: blue; font-weight: 600">Họ tên:</span>
+                                <?php echo $_SESSION['name']?>
+                            </li>
+                            <li>
+                                <span style="color: blue; font-weight: 600">Giới tính: </span> 
+                                <?php
+                                    if ($data['gender'] == 1){
+                                        echo "Nam";
+                                    }else if ($data['gender'] == 2){
+                                        echo "Nữ";
+                                    }else if ($data['gender'] == 0){
+                                        echo "<span style='color:red'>Chưa Xác định</span>";
+                                    }
+                                ?>
+                            </li>
+                            <li>
+                                <span style="color: blue; font-weight: 600">Email: </span>
+                                <?php echo $data['email']?>
+                            </li>
+                            <li>
+                                <span style="color: blue; font-weight: 600">Số điện thoại: </span>
+                                <?php
+                                    if ($data['phone_number'] == null){
+                                        echo "<span style='color:red'>Chưa Xác định</span>";
+                                    }else{
+                                        echo $data['phone_number'];
+                                    }
+                                ?>
+                            </li>
+                            <li>
+                                <span style="color: blue; font-weight: 600">Địa chỉ: </span>
+                                <?php
+                                    if ($data['phone_number'] == null){
+                                        echo "<span style='color:red'>Chưa Xác định</span>";
+                                    }else{
+                                        echo $data['address'];
+                                    }
+                                ?>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="box-btn-config">
+                        <button class="btn" onclick="changeView()">
+                            Thay đổi thông tin tài khoản
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
 </div>
