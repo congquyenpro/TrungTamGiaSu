15979d510f7b183e323a9f6940caa7aff6ec47d1
Nguyễn Công Quyền <quyen.nc207699@sis.hust.edu.vn>
2022-06-18 23:34:29 +0700

    update gd xem blog

--- src/views/content/myBlog.php
+++ src/views/content/myBlog.php
@@ -1,10 +1,11 @@
+<link rel="stylesheet" href="<?php echo $actual_link ?>/public/css/blog.css">
 <!-- Main-container section start -->
 <section class="container-main">
         <div class="container">
             <nav>
                 <ol class="my-home">
                     <li class="breadcumb-item">
-                        <a href="/home/read"><i class="fa-solid fa-house"></i></a>
+                        <a href="<?php echo $actual_link ?>/home/read"><i class="fa-solid fa-house"></i></a>
                     </li>
                     <span>/</span>
                     <li class="breadcumb-item">
