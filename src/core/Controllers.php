911665356bfb1b9f29147bbf3926752b272c27ea
Nguyễn Công Quyền <quyen.nc207699@sis.hust.edu.vn>
2022-06-20 16:21:31 +0700

    call model view getUrl

--- src/core/Controllers.php
+++ src/core/Controllers.php
@@ -5,4 +5,17 @@
             require_once "./src/models/". $model .".php";
             return new $model;
         }
+          // Hàm gọi phần view
+          protected function view($template,$view,$name_page,$data=[]){
+            $actual_link = $this->getUrl();
+            require_once "./src/views/template/". $template .".php";
+        }
+        //  nhận đường dẫn hiện tại
+        protected function getUrl(){
+            if ("$_SERVER[HTTP_HOST]" == "localhost"){
+                return "/TrungTamGiaSu/TrungTamGiaSu;
+            }
+            return "http://$_SERVER[HTTP_HOST]";
+        }
+    }
 ?>
