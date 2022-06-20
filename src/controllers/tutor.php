18f778e2b55d85ce51d8216c1f56358bcfc7b796
ND-Tuan <93861138+ND-Tuan@users.noreply.github.com>
2022-06-20 16:00:39 +0700

    update src\controllers\tutor.php

--- src/controllers/tutor.php
+++ src/controllers/tutor.php
@@ -16,3 +16,34 @@ class tutor extends Controllers{
     public function login(){
         $this->view("tutor","tutor/login","Đăng nhập",[]);
     }
+        // hàm hiển thị phần register
+        public function register(){
+            $this->view("tutor","tutor/register","Đăng kí",[]);
+        }
+        // Hiển thị phần tài khoản của tôi
+        public function my_account(){
+            if ($this->checkLogin() == false){
+                $actual_link = $this->getUrl();
+                header("Location: $actual_link/tutor/login");
+            }
+            $model = $this->model('giaSuModels');
+            $myAccount = $model->selectOne('id',$_SESSION['id']);
+            $this->view("tutor","tutor/myAccount","Tài khoản của tôi",[
+                'email'         => $myAccount['email'],
+                'phone_number'  => $myAccount['phone_number'],
+                'gender'        => $myAccount['gender'],
+                'address'       => $myAccount['address'],
+                'description'   => $myAccount['description'], 
+                'school_level'  => $myAccount['school_level'],
+                'subject'       => $myAccount['subject']
+            ]);
+        }
+        // Hiên Thị đổi mật khẩu
+        public function change_password(){
+            if ($this->checkLogin() == false){
+                $actual_link = $this->getUrl();
+                header("Location: $actual_link/tutor/login");
+            }else{
+                $this->view("tutor","editPassword","Đổi mật khẩu",[]);
+            }
+        }
