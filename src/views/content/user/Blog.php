49872e9847a765057851c1ae7f7af1d24cfc2221
Nguyễn Công Quyền <quyen.nc207699@sis.hust.edu.vn>
2022-06-22 05:42:20 +0700

    update

--- src/views/content/user/Blog.php
+++ src/views/content/user/Blog.php
@@ -14,4 +14,39 @@
                 </ol>
             </nav> 
         </div>
+        <div class="form">
+            <div class="text-heading">
+                <h1>Blog gia sư</h1>
+            </div>
+            <div class="form-fill">
+                <div class="form-all">
+                    <input id="search-input" type="search" class="search-input" size="35px" placeholder="Tìm kiếm blog" value="<?php echo $data[0]?>">
+                    <a id="search" href="tutor_blog&search=">
+                        <button class="search-btn"><i class="icon-search fa-solid fa-magnifying-glass"></i></button>
+                    </a>
+                </div>
+                <div class="category-box-group">
+                    <div class="category-box-item">
+                        <a href="#">
+                            <h3 class="category-title">Học tập</h3>
+                        </a>
+                    </div>
+                    <div class="category-box-item">
+                        <a href="#">
+                            <h3 class="category-title">Thuê gia sư</h3>
+                        </a>
+                    </div>
+                    <div class="category-box-item">
+                        <a href="#">
+                            <h3 class="category-title">Kiến thức</h3>
+                        </a>
+                    </div>
+                    <div class="category-box-item">
+                        <a href="#">
+                            <h3 class="category-title">Luyện thi cử</h3>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
         
