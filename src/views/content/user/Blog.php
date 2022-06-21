62afa3d47fe49518062c6b8cfa24ef8e8bdb3340
Nguyễn Văn Huy <nvhuy0405@gmail.com>
2022-06-22 05:09:11 +0700

    create blog function tutor

---
+++ src/views/content/user/Blog.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="<?php echo $actual_link ?>/public/css/blog.css">
+<!-- Main-container section start -->
+<section class="container-main">
+        <div class="container">
+            <nav>
+                <ol class="my-home">
+                    <li class="breadcumb-item">
+                        <a href="<?php echo $actual_link ?>/home/read"><i class="fa-solid fa-house"></i></a>
+                    </li>
+                    <span>/</span>
+                    <li class="breadcumb-item">
+                        <span>Blog khách hàng</span>
+                    </li>
+                </ol>
+            </nav> 
+        </div>
+        
