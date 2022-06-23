8ef376b836311edf355ca6ce1225b9315b4eba01
DuanLee7662 <93870205+DuanLee7662@users.noreply.github.com>
2022-06-23 11:24:48 +0700

    update check box

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -10,3 +10,14 @@ window.onscroll = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+document.getElementById("filter-clear").onclick = function () 
+{
+    // Lấy danh sách checkbox
+    var checkboxes = document.getElementsByName('name[]');
+
+    // Lặp và thiết lập Uncheck
+    for (var i = 0; i < checkboxes.length; i++){
+        checkboxes[i].checked = false;
+    }
+};
