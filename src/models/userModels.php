7f94902f686b70abe35d20c09b0324bc8ebac4fa
DuanLee7662 <93870205+DuanLee7662@users.noreply.github.com>
2022-06-23 15:47:11 +0700

    update function

--- src/models/userModels.php
+++ src/models/userModels.php
@@ -22,4 +22,58 @@ class userModels extends ConnectDB{
             return false;
         }
     }
+    // Hàm đăng nhập
+    function loginUser($email,$password){
+        // tìm kiếm dữ liệu qua email
+        $user = $user = $this->selectOne('email', $email);
+
+        // Kiểm tra xem dữ liệu có tồn tại hay không
+        if (isset($user['password'])){
+            // Kiểm tra mật khẩu
+            $verify = password_verify($password, $user['password']);
+            if ($verify){
+                // Lưu phiên hay session
+                $_SESSION['id'] = $user['id'];
+                $_SESSION['name'] = $user['name'];
+                $_SESSION['avatar'] = $user['avatar'];
+                $_SESSION['lever'] = 1;
+                return true;
+            }  
+        }else{
+            return false;
+        }
+    }
+    // hàm đổi mật khẩu
+    function ChangePass($password,$secure_pass){
+        $id = $_SESSION['id'];
+        // tìm kiếm dữ liệu qua id
+        $user = $this->selectOne('id', $id);
+
+        // Kiểm tra xem dữ liệu có tồn tại hay không
+        if (isset($user['password'])){
+            // Kiểm tra mật khẩu
+            $verify = password_verify($password, $user['password']);
+            if ($verify){
+                $sql = "UPDATE
+                        `user`
+                    SET
+                        `password` = '$secure_pass'
+                    WHERE
+                        `id` = '$id'";
+                mysqli_query($this->connection,$sql);
+                return true;
+            }  
+        }else{
+            return false;
+        }
+    }
+    // Lấy dữ liệu từ 1 hàng
+    function selectOne($key,$value){
+        // Truy vấn
+        $sql = "SELECT * FROM `user` WHERE `$key` = '$value'";
+        $user = mysqli_query($this->connection,$sql);
+        // Ép dữ liệu từ mảng về 1
+        $user = mysqli_fetch_array($user);
+        return $user;
+    }
 }
