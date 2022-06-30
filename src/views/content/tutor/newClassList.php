63bc98efb407047b7bdf4ac133f5184c2e9eddbe
Nguyễn Đức Tuân <93861138+ND-Tuan@users.noreply.github.com>
2022-06-30 20:35:52 +0700

    Update newClassList.php

--- src/views/content/tutor/newClassList.php
+++ src/views/content/tutor/newClassList.php
@@ -27,16 +27,14 @@
                 <h5>Bộ lọc</h5>
             </div>
 
-            <ul class="filter-group">
+            <form class="filter-group" method="post" action="<?php echo $actual_link?>/extra_class/find_class">
                 <li class="filter-item">
                     <span class="item-text-head">Chọn môn học <i class="fa-solid fa-angle-down"></i></span> 
                     <ul class="filter-subject-group">
-                        <li class="filter-subject-item"><input type="checkbox" name="name[]" id="square-check"><label for="square-check" class="ml-5">Toán học</label></li>
-                        <li class="filter-subject-item"><input type="checkbox" name="name[]" id="square-check2"><label for="square-check2" class="ml-5">vật lý</label></li>
-                        <li class="filter-subject-item"><input type="checkbox" name="name[]" id="square-check3"><label for="square-check3" class="ml-5">sinh học</label></li>
-                        <li class="filter-subject-item"><input type="checkbox" name="name[]" id="square-check4"><label for="square-check4" class="ml-5">ngữ văn</label></li>
-                        <li class="filter-subject-item"><input type="checkbox" name="name[]" id="square-check5"><label for="square-check5" class="ml-5">tiếng anh</label></li>
-                        <li class="filter-subject-item"><input type="checkbox" name="name[]" id="square-check6"><label for="square-check6" class="ml-5">các môn khác</label></li>
+                        <li class="filter-subject-item"><input type="radio" name="subject" value="0" id="square-check0" checked="checked"><label for="square-check0" class="ml-5">Tất cả</label></li>
+                        <?php foreach ($data[1] as $subject) { ?>
+                            <li class="filter-subject-item"><input type="radio" name="subject" value="<?=$subject['id']?>" id="square-check<?=$subject['id']?>"><label for="square-check<?=$subject['id']?>" class="ml-5"><?=$subject['name']?></label></li>
+                        <?php } ?>
                         <div class="filter-clean">
                             <div class="bodertop"></div>
                             <span id="filter-clear">Xóa</span>
@@ -47,12 +45,12 @@
                 <li class="filter-item">
                     <span class="item-text-head">Chọn cấp học <i class="fa-solid fa-angle-down"></i></span> 
                     <ul class="filter-level-group">
-                        <li class="filter-level-item"><input type="checkbox" name="name[1]" id="square-check7"><label for="square-check7" class="ml-5">mẫu giáo</label></li>
-                        <li class="filter-level-item"><input type="checkbox" name="name[1]" id="square-check8"><label for="square-check8" class="ml-5">cấp 1</label></li>
-                        <li class="filter-level-item"><input type="checkbox" name="name[1]" id="square-check9"><label for="square-check9" class="ml-5">cấp 2</label></li>
-                        <li class="filter-level-item"><input type="checkbox" name="name[1]" id="square-check10"><label for="square-check10" class="ml-5">cấp 3</label></li>
-                        <li class="filter-level-item"><input type="checkbox" name="name[1]" id="square-check11"><label for="square-check11" class="ml-5">đại học</label></li>
-                        <li class="filter-level-item"><input type="checkbox" name="name[1]" id="square-check12"><label for="square-check12" class="ml-5">người đi làm</label></li>
+                        <li class="filter-level-item"><input type="radio" name="lever" value="0" id="square-lever0" checked="checked"><label for="square-lever0" class="ml-5">Tất cả </label></li>
+                        <?php for($i = 1 ; $i <= 12 ; $i++) { ?>
+                            <li class="filter-level-item"><input type="radio" name="lever" value=<?= $i ?> id="square-lever<?= $i ?>"><label for="square-lever<?= $i ?>" class="ml-5">Lớp <?= $i ?></label></li>
+                        <?php } ?>
+                        <li class="filter-level-item"><input type="radio" name="lever" value="13" id="square-lever13"><label for="square-lever13" class="ml-5">đại học</label></li>
+                        <li class="filter-level-item"><input type="radio" name="lever" value="14" id="square-lever14"><label for="square-lever14" class="ml-5">người đi làm</label></li>
                         <div class="filter-clean">
                             <div class="bodertop"></div>
                             <span id="filter-clear1">Xóa</span>
@@ -90,17 +88,25 @@
                         <span>Tiến hành lọc</span>
                     </button>
                 </li>
-            </ul>
+            </form>
         </div>
 
         <div class="class-group">
-            <?php foreach ($data as $newClass) { ?>
+            <?php foreach ($data[0] as $newClass) { ?>
                 <div class="class-item-box">
                     <div class="class-code">C<?php echo $newClass['id']?></div>
                     <div class="container-class">
                         <p class="class-grade">
                             <i class="fa-solid fa-book"></i>
-                            <b><?php echo $newClass['name']?> - Lớp <?php echo $newClass['lever']?></b>
+                            <b><?php echo $newClass['name']?> - Lớp 
+                                <?php if ($newClass['lever'] == 14){
+                                    echo "Đi làm";
+                                }else if($newClass['lever'] == 13){
+                                    echo "Đại học";
+                                }else{
+                                    echo $newClass['lever'];
+                                } ?>
+                            </b>
                         </p>
                         <p class="class-location">
                             <i class="fa-solid fa-location-dot"></i>
